9e30736b33fe279097f9dfcfcf47be4c732c82f6
AndreyPolegaev <79646307753@yandex.ru>
2021-08-19 23:34:47 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -10,17 +10,19 @@ import java.time.format.DateTimeFormatter;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(String.format("Вакансия: %s, Дата создания: %s", href.text(), td.parent().child(5).text()));
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
-            SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
-            LocalDateTime localDateTime = sqlRuDateTimeParser.parse(td.parent().child(5).text());
-            System.out.println(formatter.format(localDateTime));
-            System.out.println("_".repeat(80));
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(String.format("Вакансия: %s, Дата создания: %s", href.text(), td.parent().child(5).text()));
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm");
+                SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+                LocalDateTime localDateTime = sqlRuDateTimeParser.parse(td.parent().child(5).text());
+                System.out.println(formatter.format(localDateTime));
+                System.out.println("_".repeat(80));
+            }
         }
     }
 }
