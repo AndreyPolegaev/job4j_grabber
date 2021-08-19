1e18decdefd48cb8355b179abdc948ce4f54738f
AndreyPolegaev <79646307753@yandex.ru>
2021-08-19 23:39:55 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,16 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+
+    private int id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private LocalDateTime created;
+}
