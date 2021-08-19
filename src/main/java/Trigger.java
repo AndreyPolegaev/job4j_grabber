8b642833a6f91e0a3c029ad7466e3c70492f7175
AndreyPolegaev <79646307753@yandex.ru>
2021-08-19 22:34:18 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/Trigger.java
+++ src/main/java/Trigger.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class Trigger {
-    public int someLogic() {
-        return 1;
+
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
+
+        System.out.println(LocalDate.now());
     }
 }
+
