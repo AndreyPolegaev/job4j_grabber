13ef3136b2332fd2d0b1f55c2f69e8d6e0bf948f
AndreyPolegaev <79646307753@yandex.ru>
2021-09-14 23:56:02 +0300

    Шаблонизатор

--- src/main/java/ru/job4j/generator/MyGenerator.java
+++ src/main/java/ru/job4j/generator/MyGenerator.java
@@ -3,8 +3,19 @@ package ru.job4j.generator;
 import java.util.Map;
 
 public class MyGenerator implements Generator {
+
     @Override
     public String produce(String template, Map<String, String> args) {
-        return null;
+        for (String temp : args.keySet()) {
+            if (template.contains(temp)) {
+                template = template.replace("${" + temp + "}", args.get(temp));
+            } else {
+                throw new UnsupportedOperationException("string doesn't this key word");
+            }
+        }
+        if (template.contains("$")) {
+            throw new UnsupportedOperationException("Key is absent");
+        }
+        return template;
     }
 }
--- src/test/java/ru/job4j/generator/MyGeneratorTest.java
+++ src/test/java/ru/job4j/generator/MyGeneratorTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.generator;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
@@ -9,13 +8,33 @@ import java.util.Map;
 
 public class MyGeneratorTest {
 
-    @Ignore
     @Test
-    public void whenSmth() {
+    public void whenAllRight() {
         Generator gen = new MyGenerator();
         String produce = gen.produce("I am a ${name}, Who are ${subject}? ", new HashMap<>(Map.of(
                 "name", "Petr Arsentev",
-                "subjet", "you"
+                "subject", "you"
+        )));
+        assertThat(produce, is("I am a Petr Arsentev, Who are you? "));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenStringDoesNotHaveKey() {
+        Generator gen = new MyGenerator();
+        String produce = gen.produce("I am a ${name}, Who are ${subject}? ", new HashMap<>(Map.of(
+                "name", "Petr Arsentev",
+                "subject", "you",
+                "key", "test"
+        )));
+        assertThat(produce, is("I am a Petr Arsentev, Who are you? "));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenHashMapDoesNotHaveStringsKey() {
+        Generator gen = new MyGenerator();
+        String produce = gen.produce("I am a ${name}, Who are ${subject}? ${absent} ", new HashMap<>(Map.of(
+                "name", "Petr Arsentev",
+                "subject", "you"
         )));
         assertThat(produce, is("I am a Petr Arsentev, Who are you? "));
     }
